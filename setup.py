3ab6eccf86f077dca2fd4d5f653b0b5d1bf3c1c1
William Grisaitis <grisaitisw@janelia.hhmi.org>
2016-11-10 12:46:42 -0500

    Adding compile wrapper for PyTED

--- setup.py
+++ setup.py
@@ -6,28 +6,29 @@ from subprocess import call
 from distutils.core import setup
 from distutils.command.build_py import build_py
 
-class copy_lib(build_py):
-
+class CMakeLib(build_py):
     def run(self):
-
         lib_extensions = [ ".so", ".dylib", ".dll" ]
-
         module_name = "pyted"
-        build_dir   = os.path.abspath(".")
+        source_dir  = os.path.abspath(".")
+        build_dir   = os.path.abspath("build_pyted")
         lib_dir     = os.path.join(build_dir, "python")
         lib_base    = os.path.join(lib_dir, module_name)
-        lib_file    = [ lib_base + e for e in lib_extensions if os.path.isfile(lib_base + e) ][0]
 
         if not self.dry_run:
+            # create our shared object file
+            call(["./compile_wrapper.sh", source_dir, build_dir, 'rename_pyted_lib'])
 
             target_dir = self.build_lib
-            print "target_dir: " + target_dir
+            print("target_dir: " + target_dir)
             module_dir = os.path.join(target_dir, module_name)
 
             # make sure the module dir exists
             self.mkpath(module_dir)
 
             # copy our library to the module dir
+            print(lib_base)
+            lib_file = [ lib_base + e for e in lib_extensions if os.path.isfile(lib_base + e) ][0]
             self.copy_file(lib_file, module_dir)
 
         # run parent implementation
@@ -40,5 +41,5 @@ setup(
     author_email='jfunke@iri.upc.edu',
     description='Python wrapper for the Tolerant Edit Distance (TED).',
     packages=['pyted'],
-    cmdclass={'build_py' : copy_lib}
+    cmdclass={'build_py' : CMakeLib}
 )
